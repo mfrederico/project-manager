edd9247823acbbce4c4f2d805e8ef5b0161d14dc
Matthew Frederico <mfrederico@gmail.com>
2011-10-10 22:15:51 -0600

    added config

--- config.php
+++ config.php
@@ -12,10 +12,6 @@ $config['session_save_path']	= $this->cwd.'/data/';
 
 //template_cache_dir:Make sure this is read and writeable by the webserver user
 $config['template_cache_dir']	= $this->cwd.'/data/';
-
-//...... Load any classes here
-$this->addClass('smarty/Smarty.class.php');
-
-include_once('lib/datastruct.php');
+$config['template_path']		= 'media/templates/';
 
 ?>
